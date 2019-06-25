6d05690d575da0554850d5b1de4da1e905ae3fdc
1whatleytay <1whatleytay@hdsb.ca>
2019-06-24 20:27:04 -0400

    Initial commit

---
+++ MafiaBot/Commands/Debug.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+
+namespace MafiaBot.Commands {
+    public class Debug : ModuleBase<SocketCommandContext> {
+        // Ping!
+        [Command("hi")]
+        public async Task Hello() {
+            await ReplyAsync("Hello!");
+        }
+
+        [Command("probe")]
+        public async Task ProbeRole([Remainder] string roleName) {
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == roleName);
+            if (role == null) {
+                await ReplyAsync($"No role named {roleName}.");
+                return;
+            }
+
+            await ReplyAsync($"{roleName}: {role.Id}");
+        }
+    }
+}
---
+++ MafiaBot/Utils.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MafiaBot {
+    public static class Utils {
+
+        // Sends an HTTP Get request. Might not be the best solution.
+        public static async Task<string> HttpGet(string address, string parameters = "") {
+            var client = new HttpClient{ BaseAddress = new Uri(address) };
+            var response = await client.GetAsync(parameters);
+
+            if (response.IsSuccessStatusCode) {
+                var result = await response.Content.ReadAsStringAsync();
+                client.Dispose();
+                return result;
+            }
+
+            client.Dispose();
+            throw new Exception("Received " + response.StatusCode
+                                        + " status code from " + address + parameters + ".");
+        }
+
+        // Discord Markdown Code.
+        public static string Code(string code, string lang = "") {
+            return "```" + lang + "\n" + code + "\n```";
+        }
+    }
+}
